1a7a24268805014b86a360848ed2c1c931023a0d
Kmphitech <kmphitech@gmail.com>
2021-09-02 09:37:31 +0530

    Added .xcframework

---
+++ VerifyWith1ID.xcframework/ios-arm64/VerifyWith1ID.framework/Headers/VerifyWith1ID.h
@@ -0,0 +1,18 @@
+//
+//  VerifyWith1ID.h
+//  VerifyWith1ID
+//
+//  Created by MAC on 06/07/21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for VerifyWith1ID.
+FOUNDATION_EXPORT double VerifyWith1IDVersionNumber;
+
+//! Project version string for VerifyWith1ID.
+FOUNDATION_EXPORT const unsigned char VerifyWith1IDVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <VerifyWith1ID/PublicHeader.h>
+
+
